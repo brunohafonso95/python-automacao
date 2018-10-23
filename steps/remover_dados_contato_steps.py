0a0ee5f9709bdbd6ce64d4d9a0e26b2ea9f591f5
Henrique Afonso <bhenriquea@indra.es>
2018-10-23 14:59:16 -0300

    fix error on feature

--- steps/remover_dados_contato_steps.py
+++ steps/remover_dados_contato_steps.py
@@ -1,33 +1,33 @@
-from behave import *
-from time import sleep
+# from behave import *
+# from time import sleep
 
-@given(u'acesso a pagina de perfil da aplicacao')
-def step_impl(context):
-    context.taskit_main_page.login()
-    context.taskit_perfil_page.acessar_secao_adicionar_contato()
+# @given(u'acesso a pagina de perfil da aplicacao')
+# def step_impl(context):
+#     context.taskit_main_page.login()
+#     context.taskit_perfil_page.acessar_secao_adicionar_contato()
    
 
-@when(u'clico no icone para excluir o email desejado')
-def step_impl(context):
-    pass
-//span[@class="title"][text()='testando@testando.com.br']/following-sibling::a
-//span[@class="title"][text()='(11) 12345-6789']/following-sibling::a
+# @when(u'clico no icone para excluir o email desejado')
+# def step_impl(context):
+#     pass
+# //span[@class="title"][text()='testando@testando.com.br']/following-sibling::a
+# //span[@class="title"][text()='(11) 12345-6789']/following-sibling::a
  
-@when(u'clico em ok para confirmar o alert')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When clico em ok para confirmar o alert')
- 
- 
-@then(u'uma toastmessage aparece com o texto Rest in peace, dear email!')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then uma toastmessage aparece com o texto Rest in peace, dear email!')
+# @when(u'clico em ok para confirmar o alert')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: When clico em ok para confirmar o alert')
  
  
-@when(u'clico no icone para excluir o telefone desejado')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When clico no icone para excluir o telefone desejado')
+# @then(u'uma toastmessage aparece com o texto Rest in peace, dear email!')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: Then uma toastmessage aparece com o texto Rest in peace, dear email!')
  
  
-@then(u'uma toastmessage aparece com o texto Rest in peace, dear phone!')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then uma toastmessage aparece com o texto Rest in peace, dear phone!')
+# @when(u'clico no icone para excluir o telefone desejado')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: When clico no icone para excluir o telefone desejado')
+ 
+ 
+# @then(u'uma toastmessage aparece com o texto Rest in peace, dear phone!')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: Then uma toastmessage aparece com o texto Rest in peace, dear phone!')
